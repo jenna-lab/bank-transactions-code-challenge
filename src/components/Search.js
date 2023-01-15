95008d81dde4c5fdfbc58aaefd5a205e5b6a2f78
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-01-15 17:33:41 +0300

    searchcomponents

---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+<div className="col mx-auto p-3">
+<input
+  type="text"
+  onChange={handleSearch}
+  placeholder="Search your Recent Transactions"
+  className="form-control form-control-sm"
+  aria-label=".form-control-sm example"
+/>
+<i className="circular search link icon"></i>
+</div>
+);
+};
+export default Search;
