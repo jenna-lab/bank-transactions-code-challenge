b758ac3db9fe14fac38e23415ed59d7a7a965f97
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-01-15 17:20:15 +0300

    functionforsubmitbutton

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,3 +7,7 @@ const Form = () => {
       [e.target.name]: e.target.value.trim(),
     });
   };
+  const [formData, setFormData] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
