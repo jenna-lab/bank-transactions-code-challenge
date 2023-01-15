7e7223e3fb3f5afc0d6e4c8741a343b09a328427
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-01-15 17:14:19 +0300

    second

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,3 +2,15 @@ import React, { useState, useEffect } from "react";
 import Form from "./Form";
 import Table from "./Table";
 import Search from "./Search";
+
+function Container1() {
+    const [transactions, setTransactions] = useState([]);
+    const [search, setSearch] = useState("");
+    useEffect(() => {
+      fetch("http://localhost:8001/transactions?q=" + search)
+        .then((res) => res.json())
+        .then((res) => {
+          setTransactions(res);
+        });
+    }, [search]);
+  
