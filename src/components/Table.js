761a9b4f2dc060c9a9f0f5a9d980aaacc772c8ed
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-01-15 17:37:28 +0300

    finaltablecomponent

---
+++ src/components/Table.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Table = ({ data }) => {
+  return (
+    <table className="table table-striped table-hover p-3">
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Description</th>
+          <th>Category</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((transaction, index) => {
+          return (
+            <tr key={index}>
+              <td>{transaction.date}</td>
+              <td>{transaction.description}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.amount}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
